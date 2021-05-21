82afc4cb9ca86bdef5b565068e2e46294577656f
Ali Onur Geven <alionur.geven@finbyte.com>
2021-05-21 15:42:51 +0200

    add 2 more bugs

--- src/main/java/com/test/sonarqubetest/model/DummyModel.java
+++ src/main/java/com/test/sonarqubetest/model/DummyModel.java
@@ -2,11 +2,16 @@ package com.test.sonarqubetest.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import org.springframework.lang.Nullable;
 
 @Data
 @AllArgsConstructor
 public class DummyModel {
 
   private String name;
-  private String id;
+  @NonNull
+  private String id = null;
+  @Nullable
+  private int score;
 }
