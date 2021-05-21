b96cb42d6300852445edb1f1b939e5b8948af6ab
Ali Onur Geven <alionur.geven@finbyte.com>
2021-05-21 15:40:39 +0200

    break the code hopefully

--- src/main/java/com/test/sonarqubetest/controller/DummyController.java
+++ src/main/java/com/test/sonarqubetest/controller/DummyController.java
@@ -3,12 +3,14 @@ package com.test.sonarqubetest.controller;
 import com.test.sonarqubetest.model.DummyModel;
 import java.util.Optional;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @RestController
 @SessionAttributes("hello") // this is non compliant
+@CrossOrigin(origins = "*")
 public class DummyController {
 
   @GetMapping("/dummyEndpoint")
